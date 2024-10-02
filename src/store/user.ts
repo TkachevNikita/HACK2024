be6c4dfef4ea534071fc535ceecd24e51c9c624f
Александр Аннамов <hash_nam@mail.ru>
2024-10-03 02:55:18 +0500

    comment state

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,11 +1,11 @@
-import type { User } from "@types/entity";
-import { atom } from "recoil";
+// import type { User } from "@types/entity";
+// import { atom } from "recoil";
 
-const userState = atom<User>({
-    key: 'userState', 
-    default: {
-        role: 'admin',
-        firstName: 'admin',
-        lastName: 'admin',
-    }, 
-});
+// const userState = atom<User>({
+//     key: 'userState', 
+//     default: {
+//         role: 'admin',
+//         firstName: 'admin',
+//         lastName: 'admin',
+//     }, 
+// });
