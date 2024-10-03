b84c10c9411d58d50147e1f6fcffca199e316921
TkachevNikita <ntkachev03@mail.ru>
2024-10-04 02:46:20 +0500

    feat: header styles

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,12 +1,26 @@
-import DefaultButton from "@components/ui/DefaultButton"
 import PageContainer from "@components/ui/PageContainer"
+import AccentButton from "@components/ui/AccentButton.tsx";
+import {Link} from "@mui/material";
+import '@styles/common/Header.css'
 
 const Header = () => {
   return (
-    <PageContainer>
-    <DefaultButton>header</DefaultButton>
-    </PageContainer>
-    
+      <header className="header">
+          <PageContainer className="header__container">
+              <a href="#" className="header__logo">
+                  <img src="https://paperbackdesign.com/wp-content/uploads/2015/04/generic-logo_150ppi-600x300px.jpg" alt="logo"/>
+              </a>
+              <nav className="header__nav">
+                  <Link href="" className="header__nav--item">События</Link>
+                  <Link href="" className="header__nav--item">Оценка</Link>
+                  <Link href="" className="header__nav--item">Контакты</Link>
+              </nav>
+
+              <AccentButton>
+                  Войти
+              </AccentButton>
+          </PageContainer>
+      </header>
   )
 }
 
