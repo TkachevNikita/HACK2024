0a0d022c56cd6ba8e49b98ae78c9e1fca4396a2b
TkachevNikita <ntkachev03@mail.ru>
2024-10-05 04:39:41 +0500

    feat: адаптив для событий

--- src/components/ui/PageContainer.tsx
+++ src/components/ui/PageContainer.tsx
@@ -7,7 +7,7 @@ const PageContainer: React.FC<BoxProps> = (props) => {
     return (
     <Box component="section" sx={{ 
       // border: '1px dashed grey',
-      display: 'flex', flexDirection: 'column', height: '100vh' }} {...rest}>
+      display: 'flex', flexDirection: 'column', height: '100vh', maxWidth: 1320, marginLeft: 'auto', marginRight: 'auto', paddingLeft: '10px', paddingRight: '10px' }} {...rest}>
         {children}
     </Box>
   );
