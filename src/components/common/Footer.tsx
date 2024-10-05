8cca68b259c6a7bf440a3755c0934804bf9d1687
TkachevNikita <ntkachev03@mail.ru>
2024-10-05 12:18:15 +0500

    feat: responsive footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -8,16 +8,20 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import '@styles/common/Footer.css'
 
 const Footer = () => {
-    return <footer className="footer" style={{ flex: '0 0 auto', marginTop: 2, }}>
-      <div style={{display: 'flex', alignItems: 'center', columnGap: 10, maxWidth: 1320}}>
-        
-        <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><YouTubeIcon/></Link>
-        <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><TelegramIcon/></Link>
-        <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><WhatsAppIcon/></Link>
-        <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><InstagramIcon/></Link>
-      </div>
-      <Copyright>© 2024 Сайт "Сцена". Все права защищены.</Copyright>
-    </footer>
+    return (
+        <footer className="footer" style={{ flex: '0 0 auto', marginTop: 2, }}>
+            <div className="footer__container">
+                <div style={{display: 'flex', alignItems: 'center', columnGap: 10, maxWidth: 1320}}>
+
+                    <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><YouTubeIcon/></Link>
+                    <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><TelegramIcon/></Link>
+                    <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><WhatsAppIcon/></Link>
+                    <Link sx={{fontSize: '12px', color: 'white', lineHeight: 0}}><InstagramIcon/></Link>
+                </div>
+                <Copyright>© 2024 Сайт "Сцена". Все права защищены.</Copyright>
+            </div>
+        </footer>
+    )
 }
 
 export default withAuthRouteCheck(Footer);
