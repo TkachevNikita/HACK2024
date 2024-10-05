31adc992bef3103cf9cc94b4f3aaba5b30d05b15
TkachevNikita <ntkachev03@mail.ru>
2024-10-05 05:03:27 +0500

    fix: more responsive for home page

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Post from "@components/common/Post.tsx";
 import {TextField} from "@mui/material";
 import '@styles/pages/Home.css';
-import Box from "@mui/material/Box";
 import Event from "@components/common/Event.tsx";
 import {eventsMock} from "@shared/mocks/events-mock.ts";
 
@@ -10,12 +9,12 @@ const Home: React.FC = () => {
     return (
         <>
             <Post/>
-            <Box sx={{display: 'flex', justifyContent: 'space-between '}}>
+            <div className="home__head">
                 <h2 className="home__title">
                     Ближайшие события
                 </h2>
-                <TextField label="Поиск ближайших событий" variant="outlined" sx={{width: '30%'}}/>
-            </Box>
+                <TextField label="Поиск ближайших событий" variant="outlined" className="home__search"/>
+            </div>
             <div className="home__events">
                 {eventsMock.map((event) =>
                     <Event key={event.id} event={event}/>
